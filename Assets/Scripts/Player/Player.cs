b268db82010ed40e73eef27a6bb46ec3d93c2e3b
Enes Nacaroğlu <enes18812009@hotmail.com>
2023-10-09 17:19:39 +0300

    basic_jump_sprite_and_draw_gizmos_for_jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,38 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private float _jumpEndTime;
-    [SerializeField] private float _jumpVelocity=5f;
-    [SerializeField] private float _jumpDuration=0.5f;
+    float _jumpEndTime;
+    [SerializeField] float _horizontalVelocity = 3;
+    [SerializeField] float _jumpVelocity = 5;
+    [SerializeField] float _jumpDuration = 0.5f;
+    [SerializeField] Sprite _jumpSprite;
+    public bool IsGrounded;
+    SpriteRenderer _spriteRenderer;
+    Sprite _defaultSprite;
+    private float _horizontal;
 
-    // Start is called before the first frame update
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultSprite = _spriteRenderer.sprite;
+    }
+
+    void OnDrawGizmos()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - _spriteRenderer.bounds.extents.y);
+        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f);
+        if (hit.collider)
+            IsGrounded = true;
+        else
+            IsGrounded = false;
+
+        _horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(_horizontal);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
         var vertical = rb.velocity.y;
 
-        if (Input.GetButtonDown("Jump"))
-        {
+        if (Input.GetButtonDown("Jump") && IsGrounded)
             _jumpEndTime = Time.time + _jumpDuration;
-        }
 
         if (Input.GetButton("Jump") && _jumpEndTime > Time.time)
-        {
             vertical = _jumpVelocity;
-        }
-            
 
-        rb.velocity= new Vector2(horizontal, vertical);
+        _horizontal *= _horizontalVelocity;
+        rb.velocity = new Vector2(_horizontal, vertical);
+        UpdateSprite();
+    }
+
+    private void UpdateSprite()
+    {
+        if (IsGrounded)
+            _spriteRenderer.sprite = _defaultSprite;
+        else
+            _spriteRenderer.sprite = _jumpSprite;
+
+        if (_horizontal > 0)
+            _spriteRenderer.flipX = false;
+        else if (_horizontal < 0)
+            _spriteRenderer.flipX = true;
     }
 }
