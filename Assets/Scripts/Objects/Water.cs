a407b55be796cbda879f8aafd653161df1baaf60
Enes Nacaroğlu <enes18812009@hotmail.com>
2024-06-30 16:58:39 +0300

    scripts_formats_fixed

--- Assets/Scripts/Objects/Water.cs
+++ Assets/Scripts/Objects/Water.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Water : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-        GetComponent<AudioSource>().Play();
+        GetComponent<AudioSource>()?.Play();
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,32 +1,33 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float _jumpEndTime;
+    
     [SerializeField] float _horizontalVelocity = 3;
     [SerializeField] float _jumpVelocity = 5;
     [SerializeField] float _jumpDuration = 0.5f;
+    [SerializeField] float _footOffSet = 0.35f;
     [SerializeField] Sprite _jumpSprite;
     [SerializeField] LayerMask _layerMask;
-    [SerializeField] float _footOffSet=0.35f;
-
     
     public bool IsGrounded;
 
-    SpriteRenderer _spriteRenderer;
+    float _jumpEndTime;
     float _horizontal;
-    Animator _animator;
     int _jumpsRemaining;
+
+    SpriteRenderer _spriteRenderer;  
+    Animator _animator; 
     AudioSource _audioSource;
+    Rigidbody _rb;
 
     void Awake()
     {
         _animator=GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _audioSource = GetComponent<AudioSource>();
+        _rb=GetComponent<Rigidbody>();
         
     }
 
@@ -54,15 +55,15 @@ public class Player : MonoBehaviour
         UpdateGrounding();
 
         _horizontal = Input.GetAxis("Horizontal");
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        var vertical = rb.velocity.y;
+        
+        var vertical = _rb.velocity.y;
 
         if (Input.GetButtonDown("Jump") && _jumpsRemaining>0)
         {
             _jumpEndTime = Time.time + _jumpDuration;
             _jumpsRemaining--;
-           _audioSource.Play();
 
+           _audioSource.Play();
             _audioSource.pitch = _jumpsRemaining > 0 ? 1 : 1.2f;
 
         }
@@ -72,7 +73,7 @@ public class Player : MonoBehaviour
             vertical = _jumpVelocity;
 
         _horizontal *= _horizontalVelocity;
-        rb.velocity = new Vector2(_horizontal, vertical);
+        _rb.velocity = new Vector2(_horizontal, vertical);
         UpdateSprite();
     }
 
