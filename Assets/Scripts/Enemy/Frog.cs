fc8dfd82faa9b43d6cae399ad7e2165b5a179803
Enes Nacaroğlu <enes18812009@hotmail.com>
2024-07-03 16:07:10 +0300

    frog_enemy_added

--- Assets/Scripts/Enemy/Frog.cs
+++ Assets/Scripts/Enemy/Frog.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Frog : MonoBehaviour
 {
     Rigidbody2D _rb;
     SpriteRenderer _spriteRenderer;
+    AudioSource _audioSource;
     Sprite _defaultSprite;
+    int _jumpsRemaining;
 
+    [SerializeField] int _Jumps = 2;
     [SerializeField] float _jumpDelay = 3;
     [SerializeField] Vector2 _jumpForce;
     [SerializeField] Sprite _jumpSprite;
@@ -15,24 +19,37 @@ public class Frog : MonoBehaviour
 
     void Awake()
     {
-        
+        _audioSource = GetComponent<AudioSource>();
         _rb= GetComponent<Rigidbody2D>();
         _spriteRenderer= GetComponent<SpriteRenderer>();
         InvokeRepeating("Jump", _jumpDelay, _jumpDelay);
         _defaultSprite = _spriteRenderer.sprite;
+        _jumpsRemaining = _Jumps;
 
     }
 
     void Jump()
     {
+        if ( _jumpsRemaining <= 0 )
+        {
+            _jumpForce *= new Vector2(-1, 1);
+            _jumpsRemaining = _Jumps;
+        }
+
+        _jumpsRemaining--;
+
         _rb.AddForce(_jumpForce);
-        _jumpForce *= new Vector2(-1,1);
-        _spriteRenderer.flipX = !_spriteRenderer.flipX;
+        _spriteRenderer.flipX = _jumpForce.x > 0;
         _spriteRenderer.sprite = _jumpSprite;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         _spriteRenderer.sprite = _defaultSprite;
+        _audioSource.Play();
+
+        if (collision.collider.CompareTag("Player"))
+            SceneManager.LoadScene(0);
+
     }
 }
--- Assets/Scripts/Objects/Water.cs
+++ Assets/Scripts/Objects/Water.cs
@@ -4,6 +4,7 @@ public class Water : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.CompareTag("Player"))
         GetComponent<AudioSource>()?.Play();
     }
 }
