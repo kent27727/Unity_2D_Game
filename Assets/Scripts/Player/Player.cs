69d8ed7fc3edf61fcf2d6fe741349206cbe65074
Enes Nacaroğlu <enes18812009@hotmail.com>
2024-07-05 11:06:35 +0300

    new_input_system_added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class Player : MonoBehaviour
     SpriteRenderer _spriteRenderer;
     AudioSource _audioSource;
     Rigidbody2D _rb;
+    PlayerInput _playerInput;
 
     float _horizontal;
     int _jumpsRemaining;
@@ -31,6 +33,7 @@ public class Player : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _audioSource = GetComponent<AudioSource>();
         _rb = GetComponent<Rigidbody2D>();
+        _playerInput = GetComponent<PlayerInput>();
     }
 
     void OnDrawGizmos()
@@ -55,11 +58,11 @@ public class Player : MonoBehaviour
     {
         UpdateGrounding();
 
-        var horizontalInput = Input.GetAxis("Horizontal");
+        var horizontalInput = _playerInput.actions["Move"].ReadValue<Vector2>().x;
 
         var vertical = _rb.velocity.y;
 
-        if (Input.GetButtonDown("Jump") && _jumpsRemaining > 0)
+        if (_playerInput.actions["Fire"].WasPerformedThisFrame() && _jumpsRemaining > 0)
         {
             _jumpEndTime = Time.time + _jumpDuration;
             _jumpsRemaining--;
@@ -68,7 +71,7 @@ public class Player : MonoBehaviour
             _audioSource.Play();
         }
 
-        if (Input.GetButton("Jump") && _jumpEndTime > Time.time)
+        if (_playerInput.actions["Fire"].ReadValue<float>() > 0 && _jumpEndTime > Time.time)
             vertical = _jumpVelocity;
 
         var desiredHorizontal = horizontalInput * _maxHorizontalSpeed;
